cc908c5139447cab40b6cd27621eb4ea5351a3be
Diego <diego060900@gmail.com>
2025-04-26 23:05:46 +0200

    Added Dash app with initial empty layout and bootstrap styling

--- pyFiles/main.py
+++ pyFiles/main.py
@@ -1,3 +1,45 @@
+import dash_bootstrap_components as dbc
+from dash import Dash, html
+
+from components.left_column import create_left_column
+from components.right_column import create_right_column
+from pyFiles.frontend.styles import STYLES, Style
+
+
+def create_app():
+    app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+    app.layout = dbc.Container(
+        [
+            # Row with Title
+            dbc.Row(
+                dbc.Col(
+                    html.H1(
+                        "Financial Transactions Dashboard",
+                        className="text-center m-0 py-2",
+                        style=STYLES[Style.APP_TITLE]
+                    )
+                )
+            ),
+
+            # Row with Left and Right Column
+            dbc.Row(
+                [
+                    create_left_column(),
+                    create_right_column()
+                ],
+                className="gx-3",
+                style={"height": "calc(100vh - 6rem)"}
+            )
+        ],
+        fluid=True,
+        className="p-3 m-0",
+        style=STYLES[Style.BODY]
+    )
+
+    return app
+
+
 if __name__ == '__main__':
-    print('start')
-    print('end')
+    app = create_app()
+    app.run(debug=True)
