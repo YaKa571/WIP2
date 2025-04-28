247aca1cef259e204ff2beee08647cfbd0052c5c
YaKa571 <yannic.kargl@gmx.net>
2025-04-28 22:35:14 +0200

    json data loaded, train_fraud to much time, set all Test Tables invisible

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ import components.component_factory as comp_factory
 from components.left_column import create_left_column
 from components.right_column import create_right_column
 from frontend.styles import STYLES, Style
+import pandas as pd
+import json
 
 data_frame_users = data_manager.read_csv_data("users_data.csv", sort_alphabetically=True)
 data_frame_transactions = data_manager.read_csv_data("transactions_data.csv", sort_alphabetically=True)
 data_frame_cards = data_manager.read_csv_data("cards_data.csv", sort_alphabetically=True)
+
+#json files, normalized to fit format
+with open(r'assets\data\mcc_codes.json', 'r', encoding='utf-8') as f:
+    data_mcc = json.load(f)
+    data_frame_mcc = pd.json_normalize(data_mcc)
+# TODO: to much loading time
+# with open(r'assets\data\train_fraud_labels.json', 'r', encoding='utf-8') as f:
+#     data_train_fraud = json.load(f)
+#     data_frame_train_fraud = pd.json_normalize(data_train_fraud)
 
 def create_app():
     external_style = [dbc.themes.BOOTSTRAP]
@@ -28,16 +39,17 @@ def create_app():
                         ),
 
                         # To have a look at a certain data table, add it here and set visible=True
-                        comp_factory.create_data_table("table1",data_frame_users , visible=True),
-                        comp_factory.create_data_table("table3", data_frame_cards, visible=True),
+                        comp_factory.create_data_table("table1",data_frame_users , visible=False),
+                        comp_factory.create_data_table("table2", data_frame_cards, visible=False),
+                        comp_factory.create_data_table("table3", data_frame_mcc, visible=False),
 
-                        # first 100.000 rows of transactions_data as table
-                        dash_table.DataTable(
-                            id='table',
-                            columns=[{"name": i, "id": i} for i in data_frame_transactions.columns],
-                            data=data_frame_transactions.head(100000).to_dict('records'),
-                            page_size=20
-                        )
+                        # first 1000 rows of transactions_data as table, uncomment to display
+                        # dash_table.DataTable(
+                        #     id='table',
+                        #     columns=[{"name": i, "id": i} for i in data_frame_transactions.columns],
+                        #     data=data_frame_transactions.head(1000).to_dict('records'),
+                        #     page_size=20
+                        # )
                     ]
                 )
             ),
@@ -63,4 +75,3 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
-    print("Hallo")
