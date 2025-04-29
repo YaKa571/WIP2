aa00b34ba394c7728f4b292832b6fb456614b918
Diego <diego060900@gmail.com>
2025-04-29 21:09:30 +0200

    Refactor KPI card creation to include dynamic content.
    Added dynamic values and icons for KPI cards to display transaction data. Updated card titles for improved clarity and standardized styles for consistency across components.

--- components/left_column.py
+++ components/left_column.py
@@ -1,14 +1,40 @@
 import dash_bootstrap_components as dbc
+from dash import html
 
+from backend.data_manager import data_frame_transactions as transactions
 from frontend.component_ids import IDs
+from frontend.icon_manager import Icons, IconID
 from frontend.styles import STYLES, Style
 
 
 def create_kpi_card(card_id: IDs, card_title: str = ""):
+    amt_transactions_str = f"{len(transactions):,}".replace(",", ".")
+    sum_transactions_str = f"${transactions['amount'].sum():,.2f}"
+    mean_transactions_str = f"${transactions['amount'].mean():,.2f}"
+
+    if card_id == IDs.KPI_CARD_AMT_TRANSACTIONS:
+        icon = html.Img(src=Icons.get_icon(IconID.CHART_PIPE), style=STYLES[Style.ICON])
+        inner = html.P(amt_transactions_str, className="card-value mb-0 pb-0")
+
+    elif card_id == IDs.KPI_CARD_SUM_OF_TRANSACTIONS:
+        icon = html.Img(src=Icons.get_icon(IconID.MONEY_DOLLAR), style=STYLES[Style.ICON])
+        inner = html.P(sum_transactions_str, className="card-value mb-0 pb-0")
+    elif card_id == IDs.KPI_CARD_AVG_TRANSACTION_AMOUNT:
+        icon = html.Img(src=Icons.get_icon(IconID.CHART_AVERAGE), style=STYLES[Style.ICON])
+        inner = html.P(mean_transactions_str, className="card-value mb-0 pb-0")
+    else:
+        icon = None
+        inner = html.P("Unsupported card_id", className="card-value mb-0 pb-0")
+
     return dbc.Card(
-        dbc.CardBody(card_title),
-        style=STYLES[Style.CARD],
-        id=card_id.value,  # Enum value is used as ID
+        dbc.CardBody([
+            icon,
+            inner,
+            html.P(card_title, className="card-title fw-bold fs-5")
+        ],
+            style=STYLES[Style.KPI_CARD_BODY]),
+        style=STYLES[Style.KPI_CARD],
+        id=str(card_id.value),  # Enum value is used as ID
         className="w-100 kpi-card"
     )
 
@@ -18,19 +44,16 @@ def create_kpi_cards():
         [
             # Separate column for each KPI card
             dbc.Col(
-                create_kpi_card(IDs.KPI_CARD_AMT_TRANSACTIONS, "Amount of Transactions"),
-                md=4,
-                className="d-flex text-center"
+                create_kpi_card(IDs.KPI_CARD_AMT_TRANSACTIONS, "Transactions"),
+                md=4
             ),
             dbc.Col(
-                create_kpi_card(IDs.KPI_CARD_SUM_OF_TRANSACTIONS, "Sum of Transactions"),
-                md=4,
-                className="d-flex text-center"
+                create_kpi_card(IDs.KPI_CARD_SUM_OF_TRANSACTIONS, "Total Value"),
+                md=4
             ),
             dbc.Col(
-                create_kpi_card(IDs.KPI_CARD_AVG_TRANSACTION_AMOUNT, "Average Transaction Amount"),
-                md=4,
-                className="d-flex text-center"
+                create_kpi_card(IDs.KPI_CARD_AVG_TRANSACTION_AMOUNT, "Avg. Transaction"),
+                md=4
             )
         ],
         className="gx-3 mb-3 kpi-cards-row"
