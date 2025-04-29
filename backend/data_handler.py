a9a00f7659c8959745414a0aa40e46a2319cb27e
Diego <diego060900@gmail.com>
2025-04-29 22:19:41 +0200

    Refactor `optimize_data` to support multiple files.
    Updated the `optimize_data` function to process multiple CSV files and convert them into Parquet format for improved efficiency. Enhanced logging and added a detailed docstring for better readability and error handling clarity.

--- backend/data_handler.py
+++ backend/data_handler.py
@@ -1,23 +1,32 @@
-import pandas as pd
 import os
 from pathlib import Path
 
+import pandas as pd
+
 DATA_DIRECTORY = Path("assets/data/")
 
-"""
-loads csv file, save as parquet
-"""
-def optimize_data(file_name: str):
-    file_path = DATA_DIRECTORY / file_name
-    base, ext = os.path.splitext(file_path)
-    parquet_path = base + '.parquet'
-    if os.path.exists(parquet_path):
-        print(f" Load from Parquet: {parquet_path}")
-        df = pd.read_parquet(parquet_path)
-    else:
-        print(f" Parquet not found. Load CSV: {file_path}")
-        df = pd.read_csv(file_path)
 
-        print(f" Save Parquet: {parquet_path}")
-        df.to_parquet(parquet_path, compression='snappy')
-    return df
+def optimize_data(*file_names: str):
+    """
+    Optimize data processing by converting a CSV file to a Parquet file for better performance and storage
+    efficiency. If a corresponding Parquet file already exists, it will be used directly instead of
+    reloading and converting the CSV file.
+
+    Args:
+        file_name (str): Name of the input CSV file to be processed.
+
+    Raises:
+        FileNotFoundError: If the specified file does not exist.
+    """
+    for file_name in file_names:
+        file_path = DATA_DIRECTORY / file_name
+        base, ext = os.path.splitext(file_path)
+        parquet_path = base + '.parquet'
+        if os.path.exists(parquet_path):
+            print(f"ℹ️ Loading from Parquet: {parquet_path}")
+        else:
+            print(f"⚠️ Parquet not found. Loading and converting CSV: {file_path}")
+            df = pd.read_csv(file_path)
+
+            print(f"ℹ️ Saved Parquet: {parquet_path}")
+            df.to_parquet(parquet_path, compression='snappy')
