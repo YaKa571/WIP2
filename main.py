ba2eb1f351ba14eb7fd380c319b749955e784c3d
YaKa571 <yannic.kargl@gmx.net>
2025-05-10 23:31:07 +0200

    added supprress_callback_exceptions=true

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ from frontend.component_ids import IDs
 
 def create_app():
     app = Dash(__name__, external_stylesheets=
-    [dbc.themes.BOOTSTRAP, "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"])
+    [dbc.themes.BOOTSTRAP, "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"],
+    suppress_callback_exceptions=True,)
 
     app.title = "Financial Transactions Dashboard"
 
