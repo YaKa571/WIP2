5c99662be9a83dde8211226e7e9ce6743296eace
Diego <diego060900@gmail.com>
2025-05-10 05:41:23 +0200

    Add new component IDs for settings and map-related features
    This commit introduces new IDs for better organization and functionality within the frontend, focusing on settings and map components. Existing IDs were adjusted for clarity, while new ones enable better callback handling and expand settings configuration capabilities.

--- frontend/component_ids.py
+++ frontend/component_ids.py
@@ -1,6 +1,10 @@
 from enum import Enum
 
 class IDs(str, Enum):
+
+    APP_STATE_STORE = "app-state-store"
+    ANIMATION_STATE_STORE = "animation-state-store"
+
     KPI_CARD_AMT_TRANSACTIONS = "kpi-card-amt-transactions"
     KPI_CARD_SUM_OF_TRANSACTIONS = "kpi-card-sum-of-transactions"
     KPI_CARD_AVG_TRANSACTION_AMOUNT = "kpi-card-avg-transaction-amount"
@@ -19,7 +23,17 @@ class IDs(str, Enum):
     TABLE_MCC = "table-mcc"
 
     MAP = "map"
+    MAP_CONTAINER = "map-container"  # Used for callback
+    MAP_SPINNER = "map-spinner"
 
-    DARK_MODE_TOGGLE = "dark-mode-toggle"
-    DARK_MODE_STORE = "dark-mode-store"
+    BUTTON_DARK_MODE_TOGGLE = "button-dark-mode-toggle"
     DASHBOARD_CONTAINER = "dashboard-container"
+
+    # Settings
+    SETTINGS_CANVAS = "settings-canvas"
+    BUTTON_SETTINGS_MENU = "button-settings-menu"
+    SETTINGS_CARD_GENERAL = "settings-card-general"
+    SETTINGS_CARD_MAP = "settings-card-map"
+    SETTING_MAP_COLOR_SCALE = "setting-map-color-scale"
+    SETTING_GENERAL_SHOW_TOOLTIPS = "setting-general-show-tooltips"
+    SETTING_GENERAL_CANVAS_PLACEMENT = "setting-general-canvas-placement"
