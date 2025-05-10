65441a18cd07399376d37b85cc41cb169c9f94c9
YaKa571 <yannic.kargl@gmx.net>
2025-05-10 23:04:05 +0200

    corrected typo

--- frontend/component_ids.py
+++ frontend/component_ids.py
@@ -38,6 +38,6 @@ class IDs(str, Enum):
     SETTING_GENERAL_SHOW_TOOLTIPS = "setting-general-show-tooltips"
     SETTING_GENERAL_CANVAS_PLACEMENT = "setting-general-canvas-placement"
 
-    # CLuster
+    # Cluster
     CLUSTER_DROPDOWN = "cluster-dropdown"
     CLUSTER_DROPDOWN_OUTPUT = "cluster-dropdown-output"
