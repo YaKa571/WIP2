b38feb05e7438d35ce0eb0ee5ea07e053a522c72
YaKa571 <yannic.kargl@gmx.net>
2025-05-10 21:41:49 +0200

    cluster: added dropdown

--- components/rightcolumn/tabs/tab_cluster.py
+++ components/rightcolumn/tabs/tab_cluster.py
@@ -1,11 +1,14 @@
-from dash import html
+from dash import html, dcc
 
 # TODO: @Yannic
+
 def create_cluster_content():
     return html.Div(
         [
             html.H1("Cluster"),
             html.P("This is the cluster page of the application."),
+            # TODO Names
+            dcc.Dropdown(['Default','Age Group','Income vs Expenditure'],'Default',id='cluster-dropdown'),
         ],
         className="tab-content-wrapper flex-fill"
     )
