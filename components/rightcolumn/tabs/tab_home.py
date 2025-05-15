2a751c1dacb377ee49647b935bb4c89745de9399
Diego <diego060900@gmail.com>
2025-05-15 06:24:06 +0200

    Refactor KPI components and add state-specific data methods
    Reorganized KPI card structure to use dynamic loading spinners and modularized HTML elements. Added methods to fetch KPI details (most valuable/visited merchant, top spending user, peak hour) with optional state-specific data. Enhanced tooltip and heading functionality for interactivity and state filtering.

--- components/rightcolumn/tabs/tab_home.py
+++ components/rightcolumn/tabs/tab_home.py
@@ -17,56 +17,75 @@ COLOR_BLUE_MAIN = "#0d6efd"
 
 
 # TODO: @Diego
+# TODO: Fix KPI Tooltips to update data based on selected states
 def create_home_content() -> html.Div:
+    """
+    Creates the main content structure for the home tab of a dashboard.
+
+    The function composes and returns a primary content container for the home
+    tab, which includes a heading, key performance indicators (KPIs),
+    and circle diagram components. These elements are assembled into a
+    single layout container styled for display in the tab content.
+
+    Returns
+    -------
+    html.Div
+        A Div element containing the formatted components for the home tab
+        content.
+    """
     return html.Div(children=[
 
+        _create_heading(),
         _create_top_kpis(),
         _create_middle_circle_diagrams()
+
     ],
         className="tab-content-inner"
     )
 
 
-def create_pie_graph(data: dict, colors=None, textinfo: str = "percent+label", showlegend: bool = True,
-                     dark_mode: bool = False) -> go.Figure:
-    if colors is None:
-        colors = ["#c65ed4", "#5d9cf8"]  # Female = pink, Male = blue
+def _create_heading() -> html.Div:
+    """
+    Creates a heading section for the home tab of the application.
 
-    textcolor = "white" if dark_mode else "black"
+    The heading includes a title, a button labeled 'ALL STATES',
+    an informational icon, and a tooltip. The header is styled
+    according to the provided classes, and the tooltip describes
+    the functionality to interact with the map for updating charts
+    and KPIs based on selected states.
 
-    labels = list(data.keys())
-    values = list(data.values())
+    Returns
+    -------
+    html.Div
+        A Dash HTML Div element containing the heading structure for
+        the home tab, including a title, button, an informational
+        icon with an interactive tooltip, and necessary CSS classes.
+    """
+    return html.Div(
+        children=[
 
-    fig = go.Figure(
-        data=[go.Pie(
-            name="",
-            labels=labels,
-            values=values,
-            hole=0.42,
-            marker=dict(colors=colors),
-            textinfo=textinfo,
-            textfont=dict(color=textcolor),
-            hovertemplate="%{label}<br>%{percent}<br>$%{value:,.2f}"
-        )],
-        layout=go.Layout(
-            margin=dict(l=0, r=0, t=0, b=0),
-            showlegend=showlegend
-        )
+            html.Button("Show all States", className="settings-button-text",
+                        id=ID.HOME_TAB_BUTTON_TOGGLE_ALL_STATES),
+            html.H4(
+                "All States",
+                id=ID.HOME_TAB_STATE_HEADING
+            ),
+            html.I(className="bi bi-info-circle-fill", id=ID.HOME_TAB_INFO_ICON),
+            dbc.Tooltip(children=[
+                "Click on any State on",
+                html.Br(),
+                "the map to update the",
+                html.Br(),
+                "charts and KPIs"
+            ],
+                target=ID.HOME_TAB_INFO_ICON,
+                is_open=False,
+                placement="bottom-end"
+            )
+
+        ],
+        className="tab-home-heading-wrapper"
     )
-
-    fig.update_layout(
-        paper_bgcolor='rgba(0,0,0,0)',  # Transparent background
-        plot_bgcolor='rgba(0,0,0,0)',
-        legend=dict(
-            x=1,  # 100% right
-            y=1,  # 100% top
-            xanchor="right",
-            yanchor="top",
-            font=dict(size=12, color="#0d6efd", weight="bold")
-        )
-    )
-
-    return fig
 
 
 def _create_top_kpis() -> html.Div:
@@ -89,106 +108,143 @@ def _create_top_kpis() -> html.Div:
     """
     return html.Div(children=[
 
-        # KPI 1: Highest Value Merchant
-        dbc.Card(children=[
-            dbc.CardHeader(children=[
+        html.Div(children=[
 
-                comp_factory.create_icon(IconID.TROPHY, cls="icon icon-small"),
-                html.P("Most Valuable Merchant", className="kpi-card-title")
+            # KPI 1: Most Value Merchant
+            dbc.Card(children=[
+                dbc.CardHeader(children=[
 
-            ],
-                className="card-header"),
-            dbc.CardBody(children=[
+                    comp_factory.create_icon(IconID.TROPHY, cls="icon icon-small"),
+                    html.P("Most Valuable Merchant", className="kpi-card-title")
 
-                html.P(
-                    f"{dm.home_kpi.most_valuable_merchant.mcc_desc}",
-                    className="kpi-card-value"),
-                html.P(f"${dm.home_kpi.most_valuable_merchant.value}",
-                       className="kpi-card-value kpi-number-value")
+                ],
+                    className="card-header"
+                ),
 
-            ],
-                className="card-body",
-                id=ID.HOME_KPI_MOST_VALUABLE_MERCHANT
+                dbc.CardBody(children=[
 
-            )],
-            className="card kpi-card",
-        ),
+                    dcc.Loading(children=[
+                        html.Div(children=[
+                            html.P("", className="kpi-card-value"),
+                            html.P("", className="kpi-card-value kpi-number-value")
+                        ],
+                            id=ID.HOME_KPI_MOST_VALUABLE_MERCHANT
+                        )
+                    ],
+                        type="circle",
+                        color=COLOR_BLUE_MAIN)
 
-        # KPI 2: Most Frequent Merchant
-        dbc.Card(children=[
-            dbc.CardHeader(children=[
+                ],
+                    className="card-body",
 
-                comp_factory.create_icon(IconID.REPEAT, cls="icon icon-small"),
-                html.P("Most Visited Merchant", className="kpi-card-title"),
-
-            ],
-                className="card-header"),
-            dbc.CardBody(children=[
-
-                html.P(
-                    f"{dm.home_kpi.most_visited_merchant.mcc_desc}",
-                    className="kpi-card-value"),
-                html.P(f"{dm.home_kpi.most_visited_merchant.visits} visits",
-                       className="kpi-card-value kpi-number-value")
+                )
 
             ],
-                className="card-body",
-                id=ID.HOME_KPI_MOST_VISITED_MERCHANT
+                className="card kpi-card",
+            ),
 
-            )],
-            className="card kpi-card",
-        ),
+            # KPI 2: Most Visited Merchant
+            dbc.Card(children=[
+                dbc.CardHeader(children=[
 
-        # KPI 3: Highest Value User
-        dbc.Card(children=[
-            dbc.CardHeader(children=[
+                    comp_factory.create_icon(IconID.REPEAT, cls="icon icon-small"),
+                    html.P("Most Visited Merchant", className="kpi-card-title"),
 
-                comp_factory.create_icon(IconID.USER_PAYING, cls="icon icon-small"),
-                html.P(children="Top Spending User", className="kpi-card-title"),
+                ],
+                    className="card-header"
+                ),
 
-            ],
-                className="card-header"),
-            dbc.CardBody(children=[
+                dbc.CardBody(children=[
 
-                html.P(
-                    f"{dm.home_kpi.top_spending_user.gender}, {dm.home_kpi.top_spending_user.current_age} years",
-                    className="kpi-card-value"),
-                html.P(f"${dm.home_kpi.top_spending_user.value}",
-                       className="kpi-card-value kpi-number-value")
+                    dcc.Loading([
+                        html.Div(children=[
 
-            ],
-                className="card-body",
-                id=ID.HOME_KPI_TOP_SPENDING_USER
+                            html.P("", className="kpi-card-value"),
+                            html.P("", className="kpi-card-value kpi-number-value")
 
-            )],
-            className="card kpi-card",
-        ),
+                        ],
+                            id=ID.HOME_KPI_MOST_VISITED_MERCHANT
+                        )
+                    ],
+                        type="circle",
+                        color=COLOR_BLUE_MAIN)
 
-        # KPI 4: Peak Hour
-        dbc.Card(children=[
-            dbc.CardHeader(children=[
-
-                comp_factory.create_icon(IconID.TIME, cls="icon icon-small"),
-                html.P(children="Peak Hour", className="kpi-card-title"),
+                ],
+                    className="card-body"
+                )
 
             ],
-                className="card-header"),
-            dbc.CardBody(children=[
+                className="card kpi-card",
+            ),
 
-                html.P(f"{dm.home_kpi.peak_hour.hour_range}", className="kpi-card-value"),
-                html.P(f"{dm.home_kpi.peak_hour.value} transactions", className="kpi-card-value kpi-number-value")
+            # KPI 3: Top Spending User
+            dbc.Card(children=[
+                dbc.CardHeader(children=[
 
+                    comp_factory.create_icon(IconID.USER_PAYING, cls="icon icon-small"),
+                    html.P(children="Top Spending User", className="kpi-card-title"),
+
+                ],
+                    className="card-header"
+                ),
+
+                dbc.CardBody(children=[
+
+                    dcc.Loading(children=[
+                        html.Div(children=[
+                            html.P("", className="kpi-card-value"),
+                            html.P("", className="kpi-card-value kpi-number-value")
+                        ],
+                            id=ID.HOME_KPI_TOP_SPENDING_USER
+                        )
+                    ],
+                        type="circle",
+                        color=COLOR_BLUE_MAIN)
+
+                ],
+                    className="card-body",
+
+                )
             ],
-                className="card-body",
-                id=ID.HOME_KPI_MOST_FREQUENT_USER
+                className="card kpi-card",
+            ),
 
-            )],
-            className="card kpi-card",
-        ),
+            # KPI 4: Peak Hour
+            dbc.Card(children=[
+                dbc.CardHeader(children=[
 
-    ],
-        className="flex-wrapper"
-    )
+                    comp_factory.create_icon(IconID.TIME, cls="icon icon-small"),
+                    html.P(children="Peak Hour", className="kpi-card-title"),
+
+                ],
+                    className="card-header"
+                ),
+
+                dbc.CardBody(children=[
+
+                    dcc.Loading(children=[
+                        html.Div(children=[
+                            html.P("", className="kpi-card-value"),
+                            html.P("", className="kpi-card-value kpi-number-value")
+                        ],
+                            id=ID.HOME_KPI_PEAK_HOUR
+                        )
+                    ],
+                        type="circle",
+                        color=COLOR_BLUE_MAIN)
+
+                ],
+                    className="card-body",
+
+                )
+            ],
+                className="card kpi-card",
+            ),
+
+        ],
+            className="flex-wrapper"
+        )
+    ])
 
 
 def _create_middle_circle_diagrams() -> html.Div:
@@ -296,3 +352,170 @@ def _create_middle_circle_diagrams() -> html.Div:
     ],
         className="flex-wrapper"
     )
+
+
+def create_pie_graph(data: dict, colors=None, textinfo: str = "percent+label", showlegend: bool = True,
+                     dark_mode: bool = False) -> go.Figure:
+    """
+    Create a pie graph visualization.
+
+    This function generates a pie chart visualization using the given data.
+    It allows customization of colors, text information display, legend visibility,
+    and a dark mode for the chart appearance.
+
+    Arguments:
+        data: dict
+            A dictionary containing labels as keys and corresponding values as values
+            for the pie chart sections.
+        colors: list, optional
+            A list of colors for the pie slices. Defaults to ["#c65ed4", "#5d9cf8"].
+        textinfo: str, optional
+            String specifying which text elements to show on the chart (e.g., 'percent+label').
+            Defaults to "percent+label".
+        showlegend: bool, optional
+            Determines whether to display the legend on the chart. Defaults to True.
+        dark_mode: bool, optional
+            Configures text color and background appearance for dark mode. Defaults to False.
+
+    Returns:
+        go.Figure
+            A Plotly Figure object representing the pie chart visualization.
+    """
+    if colors is None:
+        colors = ["#c65ed4", "#5d9cf8"]  # Female = pink, Male = blue
+
+    textcolor = "white" if dark_mode else "black"
+
+    labels = list(data.keys())
+    values = list(data.values())
+
+    fig = go.Figure(
+        data=[go.Pie(
+            name="",
+            labels=labels,
+            values=values,
+            hole=0.42,
+            marker=dict(colors=colors),
+            textinfo=textinfo,
+            textfont=dict(color=textcolor),
+            hovertemplate="%{label}<br>%{percent}<br>$%{value:,.2f}"
+        )],
+        layout=go.Layout(
+            margin=dict(l=0, r=0, t=0, b=0),
+            showlegend=showlegend
+        )
+    )
+
+    fig.update_layout(
+        paper_bgcolor='rgba(0,0,0,0)',  # Transparent background
+        plot_bgcolor='rgba(0,0,0,0)',
+        legend=dict(
+            x=1,  # 100% right
+            y=1,  # 100% top
+            xanchor="right",
+            yanchor="top",
+            font=dict(size=12, color="#0d6efd", weight="bold")
+        )
+    )
+
+    return fig
+
+
+def get_most_valuable_merchant_details(state: str = None) -> list:
+    """
+    Fetches and generates HTML components that display the most valuable merchant's
+    details. The function retrieves the most profitable merchant's description and
+    value for a given state, wraps them in HTML components, and returns them as
+    elements in a list.
+
+    Parameters:
+        state (str, optional): The state for which the most valuable merchant's
+        information should be retrieved. Defaults to None.
+
+    Returns:
+        list: A list containing two HTML components. The first component displays
+        the merchant's description, and the second displays the monetary value
+        associated with the merchant.
+
+    """
+    one = html.P(
+        f"{dm.get_most_valuable_merchant(state).mcc_desc}",
+        className="kpi-card-value")
+    two = html.P(f"${dm.get_most_valuable_merchant(state).value}",
+                 className="kpi-card-value kpi-number-value")
+
+    return [one, two]
+
+
+def get_most_visited_merchant_details(state: str = None) -> list:
+    """
+    Fetches and returns the details of the most visited merchant for a given state.
+
+    The function retrieves details of the most visited merchant, such as the merchant's
+    description and the number of visits, and formats them as HTML components.
+
+    Parameters:
+        state (str, optional): The state for which the most visited merchant's
+        details are to be fetched. Defaults to None.
+
+    Returns:
+        list: A list containing two HTML components. The first component includes the
+        description of the most visited merchant, and the second component includes
+        the number of visits.
+    """
+    one = html.P(
+        f"{dm.get_most_visited_merchant(state).mcc_desc}",
+        className="kpi-card-value")
+    two = html.P(f"{dm.get_most_visited_merchant(state).visits} visits",
+                 className="kpi-card-value kpi-number-value")
+
+    return [one, two]
+
+
+def get_top_spending_user_details(state: str = None) -> list:
+    """
+    Retrieves details of the top spending user in a given state. This function fetches the
+    top spending user's details, such as gender, age, and spending value, formats them
+    into presentable components, and returns them as a list.
+
+    Parameters:
+    state: str, optional
+        The state for which to retrieve the top spending user details. If not provided,
+        data is fetched for all states.
+
+    Returns:
+    list
+        A list of top spending user details, including formatted gender, age, and
+        spending value.
+    """
+    one = html.P(
+        f"{dm.get_top_spending_user(state).gender}, {dm.get_top_spending_user(state).current_age} years",
+        className="kpi-card-value")
+    two = html.P(f"${dm.get_top_spending_user(state).value}",
+                 className="kpi-card-value kpi-number-value")
+
+    return [one, two]
+
+
+def get_peak_hour_details(state: str = None) -> list:
+    """
+    Fetches and returns the details of peak hour metrics for a given state. This includes
+    the hour range during which peak activity occurs and the number of transactions
+    recorded during that period. The function constructs HTML components containing
+    these details for consistent rendering in the application.
+
+    Args:
+        state (str, optional): The state identifier for which peak hour details are
+            requested. Defaults to None.
+
+    Returns:
+        list: A list of HTML paragraph elements containing the peak hour range and the
+            transaction count information.
+    """
+    one = html.P(
+        f"{dm.get_peak_hour(state).hour_range}",
+        className="kpi-card-value")
+    two = html.P(f"{dm.get_peak_hour(state).value} transactions",
+                 className="kpi-card-value kpi-number-value")
+
+    return [one, two]
