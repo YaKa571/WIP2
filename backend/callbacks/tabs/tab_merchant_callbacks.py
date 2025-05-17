f162c4116a9c48d7d6a04f61a48af014743e75b9
YaKa571 <yannic.kargl@gmx.net>
2025-05-17 09:51:23 +0200

    merchant: added imports

---
+++ backend/callbacks/tabs/tab_merchant_callbacks.py
@@ -0,0 +1,1 @@
+from frontend.component_ids import ID
--- components/rightcolumn/tabs/tab_merchant.py
+++ components/rightcolumn/tabs/tab_merchant.py
@@ -1,5 +1,5 @@
 from dash import html
-
+from frontend.component_ids import ID
 
 # TODO: Free...(Yannic): Idee untere Teil der Seite wie in Skizze, oben Verteilung der Händlerkategorien und Aufschlüsselung (evtl. als Popup)
 def create_merchant_content():
