438808ad86343e0a6a95347c2a800c6089a7bdad
YaKa571 <yannic.kargl@gmx.net>
2025-05-18 10:15:08 +0200

    merchant: import of mcc_codes.json

--- backend/data_setup/tabs/tab_merchant_data_setup.py
+++ backend/data_setup/tabs/tab_merchant_data_setup.py
@@ -1,6 +1,21 @@
+import pandas as pd
+from backend.data_manager import DataManager
+import json
 """
 contains data setup for Merchant tab
 """
+
+# Data Files
+dm: DataManager = DataManager.get_instance()
+my_transactions = dm.df_transactions
+# mcc code
+with open("assets/data/mcc_codes.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+my_mcc = pd.DataFrame(list(data.items()), columns=["mcc_code", "merchant_group"])
+my_mcc["mcc_code"] = my_mcc["mcc_code"].astype(int)
+# print(my_mcc.head())
+
+
 def get_most_frequently_used_merchant_group():
     group_return = "group 1"
     count_return = "count 1"
