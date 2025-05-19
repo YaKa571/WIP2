f22a7a6cd5044ad242b8070ae42838975b2a1b16
Diego <diego060900@gmail.com>
2025-05-19 23:31:06 +0200

    Update Fraud tab layout with heading and tooltip
    Refactor Fraud tab content to include a structured heading layout. Added an info icon with a tooltip for additional context, leveraging Dash and Bootstrap components. Enhanced modularity by introducing a helper function for the heading creation.

--- components/rightcolumn/tabs/tab_fraud.py
+++ components/rightcolumn/tabs/tab_fraud.py
@@ -1,12 +1,53 @@
+import dash_bootstrap_components as dbc
 from dash import html
+
+from components.factories import component_factory as comp_factory
+from frontend.component_ids import ID
+
+
+# Info: Edit grid layout in assets/css/tabs.css
 
 
 # TODO: Free...
 def create_fraud_content():
     return html.Div(
-        [
-            html.H1("Fraud"),
-            html.P("This is the fraud page of the application."),
-        ],
-        className="tab-content-inner"
+        children=[
+
+        _create_heading()
+
+    ],
+        className="tab-content-inner fraud-tab"
     )
+
+
+def _create_heading() -> html.Div:
+    """
+    Generates a heading layout for the Fraud tab using Dash components.
+
+    The layout includes a dummy placeholder, a header, an information icon, and a tooltip
+    with additional information.
+
+    Returns:
+        html.Div: A Div element containing the structured layout for the Fraud tab heading.
+
+    """
+    return html.Div(
+        className="tab-heading-wrapper",
+        children=[
+
+            html.P(),  # Dummy element
+            html.H4("Fraud", id=ID.FRAUD_TAB_HEADING),
+            comp_factory.create_info_icon(ID.FRAUD_TAB_INFO_ICON),
+            dbc.Tooltip(
+                target=ID.FRAUD_TAB_INFO_ICON,
+                is_open=False,
+                placement="bottom-end",
+                children=[
+                    "Placeholder for",
+                    html.Br(),
+                    "the tooltip",
+                    html.Br(),
+                    "..."
+                ])
+
+        ])
