32f6b820622008a4a4a74d7004d10e7ab76289b7
Diego <diego060900@gmail.com>
2025-05-23 03:09:38 +0200

    Add TODO for visualizing credit card limits in a bar chart
    A TODO comment was added to outline the enhancement of displaying a horizontal bar chart for credit card limits. This provides a clearer and more intuitive view of individual card values for the user.

--- components/rightcolumn/tabs/tab_user.py
+++ components/rightcolumn/tabs/tab_user.py
@@ -13,6 +13,10 @@ MODEBAR_CONFIG = dict(
 )
 
 
+# TODO: Instead of only showing credit limit also show a horizontal bar chart with
+#  single values of each credit card of the user:
+#  | Card 1  |    Card 2    | Card 3 |         Card 4         |
+#  ^ Something like that ^
 def create_user_content() -> html.Div:
     """
     Creates the user content section of a web application using Dash framework.
