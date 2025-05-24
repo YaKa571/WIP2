13c4d7d792189893495194a30f62753fd47b5256
YaKa571 <yannic.kargl@gmx.net>
2025-05-24 13:28:35 +0200

    CLUSTER : data file imported from merchant

--- backend/data_setup/tabs/tab_cluster_data_setup.py
+++ backend/data_setup/tabs/tab_cluster_data_setup.py
@@ -1,9 +1,8 @@
 import json
-
 import pandas as pd
 import datetime
 from sklearn.cluster import KMeans
-
+from backend.data_setup.tabs.tab_merchant_data_setup import get_my_transactions_mcc_users
 """
 contains data setup for Cluster tab
 """
@@ -13,6 +12,8 @@ with open("assets/data/mcc_codes.json", "r", encoding="utf-8") as file:
 my_mcc = pd.DataFrame(list(data.items()), columns=["mcc", "merchant_group"])
 my_mcc["mcc"] = my_mcc["mcc"].astype(int)
 
+my_data_file = get_my_transactions_mcc_users()
+print(my_data_file.head())
 def get_cluster_merchant_group_dropdown():
     my_df = my_mcc
     my_list = sorted(my_df['merchant_group'].unique().tolist())
