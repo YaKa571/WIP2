4e4c17491dc9cea71ff4ef980891dd11dd36f34c
Diego <diego060900@gmail.com>
2025-05-28 15:47:13 +0200

    Add new color constants and AGE_GROUP_COLORS array to constants.py

--- components/constants.py
+++ components/constants.py
@@ -4,3 +4,18 @@ COLOR_BLUE_MAIN = "#2563eb"
 COLOR_FEMALE_PINK = "#c65ed4"
 TEXT_COLOR_LIGHT = "#222c3a"
 TEXT_COLOR_DARK = "#e0e6ed"
+GREEN_LIGHT = "#259d60"
+GREEN_DARK = "#81C784"
+COLOR_ONLINE = "#00bcd4"
+COLOR_INSTORE = "#fb8c00"
+AGE_GROUP_COLORS = [
+    "#00b894",
+    "#00cec9",
+    "#0984e3",
+    "#6c5ce7",
+    "#fdcb6e",
+    "#e17055",
+    "#d35400",
+    "#636e72",
+    "#b2bec3",
+]
